8c9ff6239fa53f972478bd168ff0a9dae36a0ada
Seth Gibelyou <slgibelyou@gmail.com>
2023-02-04 01:07:17 -0600

    add real implementation for `signIn` and `signOut`

--- lib/datamine_client.dart
+++ lib/datamine_client.dart
@@ -1,7 +1,29 @@
 library datamine_client;
 
+import 'dart:async';
+
+import 'package:google_sign_in/google_sign_in.dart';
+
 /// A client to interact with a DataMine's storage.
 class DatamineClient {
-  String signIn() => "User Name";
-  void signOut() {}
+  final _googleSignIn = GoogleSignIn(scopes: [
+    'profile',
+    'https://www.googleapis.com/auth/drive',
+  ]);
+
+  Future<void> signIn() async {
+    try {
+      await _googleSignIn.signInSilently(suppressErrors: false);
+    } catch (_) {
+      await _googleSignIn.signIn();
+    }
+  }
+
+  Future<void> signOut() async {
+    await _googleSignIn.disconnect();
+  }
+
+  Future<bool> get isLoggedIn => _googleSignIn.isSignedIn();
+  String? get displayName => _googleSignIn.currentUser?.displayName;
+  String? get photoUrl => _googleSignIn.currentUser?.photoUrl;
 }
--- test/datamine_client_test.dart
+++ test/datamine_client_test.dart
@@ -1,11 +1,51 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:io';
 
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_sign_in_platform_interface/google_sign_in_platform_interface.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'datamine_client_test.mocks.dart';
 import 'package:datamine_client/datamine_client.dart';
 
+@GenerateMocks(<Type>[GoogleSignInPlatform])
 void main() {
-  test('adds one to input values', () {
+  late MockGoogleSignInPlatform mockPlatform;
+  final defaultUser = GoogleSignInUserData(
+    email: 'john.doe@gmail.com',
+    id: '8162538176523816253123',
+    photoUrl: 'https://lh5.googleusercontent.com/photo.jpg',
+    displayName: 'John Doe',
+    serverAuthCode: '789',
+  );
+
+  setUp(() {
+    mockPlatform = MockGoogleSignInPlatform();
+    when(mockPlatform.isMock).thenReturn(true);
+    when(mockPlatform.signIn()).thenAnswer((Invocation _) async => defaultUser);
+    when(mockPlatform.signInSilently())
+        .thenAnswer((Invocation _) async => defaultUser);
+
+    GoogleSignInPlatform.instance = mockPlatform;
+  });
+
+  test('state changes on sign out', () async {
     final client = DatamineClient();
-    expect(client.signIn(), "User Name");
-    client.signOut();
+    await client.signIn();
+    expect(client.displayName, defaultUser.displayName);
+    expect(client.photoUrl, defaultUser.photoUrl);
+
+    await client.signOut();
+    expect(client.displayName, null);
+    expect(client.photoUrl, null);
+  });
+
+  test('falls back after failed silent sign in', () async {
+    when(mockPlatform.signInSilently()).thenThrow(Exception('Silent fails'));
+    final client = DatamineClient();
+    await client.signIn();
+
+    verify(mockPlatform.signInSilently());
+    verify(mockPlatform.signIn());
   });
 }
